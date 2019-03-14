dc115cf16b45c01f53c09bd5b3715c85d4831dac
Dan <danmontielh@gmail.com>
2019-03-14 00:58:18 -0600

    resolved

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,21 @@
-#write your code here
+def echo text
+    text
+end
+
+def shout text
+    text.upcase
+end
+
+def repeat (text, number = 2)
+array_text = []
+    i = 0
+    loop do
+        i+=1
+        array_text.push(text)
+        if i == number
+            break
+        end
+    end
+array_text.join(" ")
+end
+
