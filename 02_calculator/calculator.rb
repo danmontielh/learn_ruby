59d3cfee3d1e194c7d43b409804e75140cbd4647
Dan <danmontielh@gmail.com>
2019-03-14 00:15:06 -0600

    some results fixed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add (a, b)
+    sum = a + b
+    sum
+end
+
+def subtract (a, b)
+    subt = a - b
+    subt
+end
+
+def sum (arr)
+result = 0
+    for value in arr do
+        result+=value
+    end
+result
+end
